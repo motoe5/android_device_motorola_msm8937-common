c242fd35facf31499003bde1573f44c87de54d77
Sohamlad7 <sohamlad7@gmail.com>
2019-10-09 12:43:57 +0530

    msm8937-common: enable DEXPREOPT

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -72,14 +72,8 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_NO_CHARGER_LED := true
 
 # Enable dex-preoptimization to speed up first boot sequence
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-    endif
-  endif
-endif
+WITH_DEXPREOPT := true
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
 
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
